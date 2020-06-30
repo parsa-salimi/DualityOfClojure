e3af90b9a660dc2cef154cf04dd036fa1d463980
parsa-salimi <parsa2000kurd@gmail.com>
2020-06-30 00:06:10 -0400

    bugfix

--- src/my_proj/fk.clj
+++ src/my_proj/fk.clj
@@ -1,14 +1,15 @@
-(load "my_proj/dnf")
-(require '[clojure.set :as set])
 (ns fk)
+(require '[clojure.set :as set])
+(load "my_proj/dnf")
+
 
 (defn easycases [f g]
     (defn samevars [f g]
         (def var-f (dnf/vars f))
         (def var-g (dnf/vars g))
-        (let [(fminusg (set/difference var-f var-g))
-              (gminusf (set/difference var-g var-f))
-              (diff (if (not (empty? fminusg)) (list 'f fminusg) (list 'g gminusf)))]
+        (let [fminusg (set/difference var-f var-g)
+              gminusf (set/difference var-g var-f)
+              diff (if (not (empty? fminusg)) (list 'f fminusg) (list 'g gminusf))]
             (if (= var-f var-g) true
                 (map #(list % (first diff)) (second diff)))))
     (list (samevars f g)))
